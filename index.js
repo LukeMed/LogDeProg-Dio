810633151ce7f31134b376bd31d7452a845238b3
LukeMed <luke.lam1988@gmail.com>
2025-07-29 12:13:51 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let nomesPokemon = [
+
+  ["poochyena", "M", 2],
+  ["zigzagoon", "F", 2],
+  ["dragonite", "M", 5],
+  ["dragonite", "F", 5],
+  ["dragonite", "F", 5],
+  ["poochyena", "F", 3],
+  ["wurmple", "M", 2]
+  
+]
+const msg = " foi cadastrado com sucesso!!"
+
+console.log("O pokemon " + nomesPokemon[1][0] + " do sexo " + nomesPokemon[1][1] + " que está no nível " + nomesPokemon[1][2] + msg)
